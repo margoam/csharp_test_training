6bffe283e55855666bcab79be96a6c1f20d46db8
margoam <mozheliukrita@gmail.com>
2022-10-01 20:27:47 +0200

    Changed names

--- addressbook-web-tests/addressbook-web-tests/GroupCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/GroupCreationTests.cs
@@ -7,21 +7,21 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 
-namespace address_book_web_tests
+namespace AddressbookWebTests
 {
-    [TestFixture]
-    public class TestAddGroup
+    [TestFixture] // attribute
+    public class GroupCreationTests
     {
-        private IWebDriver driver;
+        private IWebDriver driver; 
         private StringBuilder verificationErrors;
-        private string baseURL;
+        private string baseURL;// field
         private bool acceptNextAlert = true;
 
         [SetUp]
         public void SetupTest()
         {
             driver = new FirefoxDriver();
-            baseURL = "http://localhost/addressbook/";
+            baseURL = "http://localhost/addressbook";
             verificationErrors = new StringBuilder();
         }
 
@@ -40,7 +40,7 @@ namespace address_book_web_tests
         }
 
         [Test]
-        public void TheAddGroupTest()
+        public void GroupCreationTest()
         {
             driver.Navigate().GoToUrl(baseURL);
             driver.FindElement(By.Name("user")).Click();
@@ -51,47 +51,7 @@ namespace address_book_web_tests
             driver.FindElement(By.Name("pass")).Clear();
             driver.FindElement(By.Name("pass")).SendKeys("secret");
             driver.FindElement(By.XPath("//input[@value='Login']")).Click();
-            driver.FindElement(By.Name("new")).Click();
-            driver.FindElement(By.Name("group_name")).Click();
-            driver.FindElement(By.Name("group_name")).Clear();
-            driver.FindElement(By.Name("group_name")).SendKeys("test3");
-            driver.FindElement(By.Name("group_header")).Click();
-            driver.FindElement(By.Name("group_header")).Clear();
-            driver.FindElement(By.Name("group_header")).SendKeys("dsffsd");
-            driver.FindElement(By.Name("group_footer")).Click();
-            driver.FindElement(By.Name("group_footer")).Clear();
-            driver.FindElement(By.Name("group_footer")).SendKeys("sggsdgs");
-            driver.FindElement(By.Name("submit")).Click();
-            driver.FindElement(By.LinkText("group page")).Click();
-            driver.FindElement(By.LinkText("Logout")).Click();
-            driver.FindElement(By.Name("user")).Click();
-            driver.FindElement(By.Id("username")).Click();
-            driver.FindElement(By.Id("username")).Clear();
-            driver.FindElement(By.Id("username")).SendKeys("Administrator");
-            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
-            driver.FindElement(By.Id("password")).Clear();
-            driver.FindElement(By.Id("password")).SendKeys("root");
-            driver.FindElement(By.XPath("//form[@id='login-form']/fieldset/div/label/span")).Click();
-            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
-            driver.FindElement(By.XPath("//div[@id='navbar-container']/div[2]/ul/li[3]/a/span")).Click();
-            driver.FindElement(By.LinkText("Logout")).Click();
-            driver.FindElement(By.XPath("//div[@id='breadcrumbs']/ul/li/span")).Click();
-            acceptNextAlert = true;
-            driver.FindElement(By.Id("363")).Click();
-            driver.FindElement(By.XPath("//input[@value='Delete']")).Click();
-            Assert.IsTrue(Regex.IsMatch(CloseAlertAndGetItsText(), "^Delete 1 addresses[\\s\\S]$"));
-            driver.FindElement(By.Name("password")).Click();
-            driver.FindElement(By.XPath("//button[@type='submit']")).Click();
-            driver.Navigate().GoToUrl("https://open-eshop.stqa.ru/oc-panel/auth/login");
-            driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='My Purchases'])[1]/preceding::a[2]")).Click();
-            driver.FindElement(By.LinkText("Logout")).Click();
-            driver.Navigate().GoToUrl("https://open-eshop.stqa.ru//");
-            driver.FindElement(By.Name("user")).Click();
-            driver.FindElement(By.Name("pass")).Click();
-            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
-            driver.Navigate().GoToUrl("http://localhost/addressbook/index.php");
             driver.FindElement(By.LinkText("groups")).Click();
-            driver.Navigate().GoToUrl("http://localhost/addressbook/group.php");
             driver.FindElement(By.Name("new")).Click();
             driver.FindElement(By.Name("group_name")).Click();
             driver.FindElement(By.Name("group_name")).Clear();
@@ -103,8 +63,8 @@ namespace address_book_web_tests
             driver.FindElement(By.Name("group_footer")).Clear();
             driver.FindElement(By.Name("group_footer")).SendKeys("q");
             driver.FindElement(By.Name("submit")).Click();
-            driver.FindElement(By.LinkText("group page")).Click();
             driver.FindElement(By.LinkText("Logout")).Click();
+            
         }
         private bool IsElementPresent(By by)
         {
@@ -137,7 +97,7 @@ namespace address_book_web_tests
             try
             {
                 IAlert alert = driver.SwitchTo().Alert();
-                string alertText = alert.Text;
+                string alertText = alert.Text; // local variable
                 if (acceptNextAlert)
                 {
                     alert.Accept();
