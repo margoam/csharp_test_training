eedc73c61292ff1e30dee436217b4c274606e74d
margoam <mozheliukrita@gmail.com>
2022-10-03 22:13:01 +0200

    deleted wrong realization

--- addressbook-web-tests/addressbook-web-tests/ContactCreationTest.cs
+++ addressbook-web-tests/addressbook-web-tests/ContactCreationTest.cs
@@ -12,58 +12,57 @@ namespace addressbook_web_tests
     [TestFixture]
     public class ContactCreationtest
     {
-        Initialization init = new Initialization();
 
         [Test]
         public void ContactCreationTest()
         {
-            init.OpenHomePage();
-            init.Login(new AccountData("admin", "secret"));
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
             GoToContactCreationpage();
             FillContactForm(new ContactData("First_name", "Last_name", "Company", "Address", "1123123"));
             SubmitContactCreation();
-            init.LogOut();
+            LogOut();
         }
 
         private void SubmitContactCreation()
         {
-            init.driver.FindElement(By.XPath("//div[@id='content']/form/input[21]")).Click();
+            driver.FindElement(By.XPath("//div[@id='content']/form/input[21]")).Click();
         }
 
         private void FillContactForm(ContactData contactdata)
         {
-            init.driver.FindElement(By.Name("firstname")).Click();
-            init.driver.FindElement(By.Name("firstname")).Clear();
-            init.driver.FindElement(By.Name("firstname")).SendKeys(contactdata.Firstname);
-            init.driver.FindElement(By.Name("middlename")).Click();
-            init.driver.FindElement(By.Name("middlename")).Clear();
-            init.driver.FindElement(By.Name("middlename")).SendKeys(contactdata.Middlename);
-            init.driver.FindElement(By.Name("lastname")).Click();
-            init.driver.FindElement(By.Name("lastname")).Clear();
-            init.driver.FindElement(By.Name("lastname")).SendKeys(contactdata.Lastname);
-            init.driver.FindElement(By.Name("nickname")).Click();
-            init.driver.FindElement(By.Name("nickname")).Clear();
-            init.driver.FindElement(By.Name("nickname")).SendKeys(contactdata.Nickname);
-            init.driver.FindElement(By.Name("company")).Click();
-            init.driver.FindElement(By.Name("company")).Clear();
-            init.driver.FindElement(By.Name("company")).SendKeys(contactdata.Company);
-            init.driver.FindElement(By.Name("address")).Click();
-            init.driver.FindElement(By.Name("address")).Clear();
-            init.driver.FindElement(By.Name("address")).SendKeys(contactdata.Address);
-            init.driver.FindElement(By.Name("home")).Click();
-            init.driver.FindElement(By.Name("home")).Clear();
-            init.driver.FindElement(By.Name("home")).SendKeys(contactdata.Hometel);
-            init.driver.FindElement(By.Name("mobile")).Click();
-            init.driver.FindElement(By.Name("mobile")).Clear();
-            init.driver.FindElement(By.Name("mobile")).SendKeys(contactdata.MobTel);
-            init.driver.FindElement(By.Name("work")).Click();
-            init.driver.FindElement(By.Name("work")).Clear();
-            init.driver.FindElement(By.Name("work")).SendKeys(contactdata.WorkTel);
+            driver.FindElement(By.Name("firstname")).Click();
+            driver.FindElement(By.Name("firstname")).Clear();
+            driver.FindElement(By.Name("firstname")).SendKeys(contactdata.Firstname);
+            driver.FindElement(By.Name("middlename")).Click();
+            driver.FindElement(By.Name("middlename")).Clear();
+            driver.FindElement(By.Name("middlename")).SendKeys(contactdata.Middlename);
+            driver.FindElement(By.Name("lastname")).Click();
+            driver.FindElement(By.Name("lastname")).Clear();
+            driver.FindElement(By.Name("lastname")).SendKeys(contactdata.Lastname);
+            driver.FindElement(By.Name("nickname")).Click();
+            driver.FindElement(By.Name("nickname")).Clear();
+            driver.FindElement(By.Name("nickname")).SendKeys(contactdata.Nickname);
+            driver.FindElement(By.Name("company")).Click();
+            driver.FindElement(By.Name("company")).Clear();
+            driver.FindElement(By.Name("company")).SendKeys(contactdata.Company);
+            driver.FindElement(By.Name("address")).Click();
+            driver.FindElement(By.Name("address")).Clear();
+            driver.FindElement(By.Name("address")).SendKeys(contactdata.Address);
+            driver.FindElement(By.Name("home")).Click();
+            driver.FindElement(By.Name("home")).Clear();
+            driver.FindElement(By.Name("home")).SendKeys(contactdata.Hometel);
+            driver.FindElement(By.Name("mobile")).Click();
+            driver.FindElement(By.Name("mobile")).Clear();
+            driver.FindElement(By.Name("mobile")).SendKeys(contactdata.MobTel);
+            driver.FindElement(By.Name("work")).Click();
+            driver.FindElement(By.Name("work")).Clear();
+            driver.FindElement(By.Name("work")).SendKeys(contactdata.WorkTel);
         }
 
         private void GoToContactCreationpage()
         {
-            init.driver.FindElement(By.LinkText("add new")).Click();
+            driver.FindElement(By.LinkText("add new")).Click();
         }
 
     }
--- addressbook-web-tests/addressbook-web-tests/GroupCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/GroupCreationTests.cs
@@ -10,49 +10,49 @@ using OpenQA.Selenium.Support.UI;
 namespace addressbook_web_tests
 {
     [TestFixture] // attribute
-    public class GroupCreationTests
+    public class GroupCreationTests 
     {
-        Initialization init = new Initialization();
+        
 
         [Test]
         public void GroupCreationTest()
         {
-            init.OpenHomePage();
-            init.Login(new AccountData("admin", "secret"));
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
             GoToGroupsPage();
             CreationNewGroup();
             FillGroupform(new GroupData("name", "header", "footer"));
             SubmitGroupCreation();
-            init.LogOut();
+            LogOut();
 
         }
 
         private void SubmitGroupCreation()
         {
-            init.driver.FindElement(By.Name("submit")).Click();
+            driver.FindElement(By.Name("submit")).Click();
         }
 
         private void FillGroupform(GroupData groupdata)
         {
-            init.driver.FindElement(By.Name("group_name")).Click();
-            init.driver.FindElement(By.Name("group_name")).Clear();
-            init.driver.FindElement(By.Name("group_name")).SendKeys(groupdata.Name);
-            init.driver.FindElement(By.Name("group_header")).Click();
-            init.driver.FindElement(By.Name("group_header")).Clear();
-            init.driver.FindElement(By.Name("group_header")).SendKeys(groupdata.Header);
-            init.driver.FindElement(By.Name("group_footer")).Click();
-            init.driver.FindElement(By.Name("group_footer")).Clear();
-            init.driver.FindElement(By.Name("group_footer")).SendKeys(groupdata.Footer);
+            driver.FindElement(By.Name("group_name")).Click();
+            driver.FindElement(By.Name("group_name")).Clear();
+            driver.FindElement(By.Name("group_name")).SendKeys(groupdata.Name);
+            driver.FindElement(By.Name("group_header")).Click();
+            driver.FindElement(By.Name("group_header")).Clear();
+            driver.FindElement(By.Name("group_header")).SendKeys(groupdata.Header);
+            driver.FindElement(By.Name("group_footer")).Click();
+            driver.FindElement(By.Name("group_footer")).Clear();
+            driver.FindElement(By.Name("group_footer")).SendKeys(groupdata.Footer);
         }
 
         private void CreationNewGroup()
         {
-            init.driver.FindElement(By.Name("new")).Click();
+            driver.FindElement(By.Name("new")).Click();
         }
 
         private void GoToGroupsPage()
         {
-            init.driver.FindElement(By.LinkText("groups")).Click();
+            driver.FindElement(By.LinkText("groups")).Click();
         }
 
 
