0979e20fcf9327101814d5ddd34276b43ebc0dff
margoam <mozheliukrita@gmail.com>
2022-10-03 20:13:07 +0200

    Added new changes

---
+++ addressbook-web-tests/addressbook-web-tests/AccountData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace addressbook_web_tests
+{
+    public class AccountData
+    {
+        private string username;
+        private string password;
+
+        public AccountData(string username, string password) // constructor
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+
+            set
+            {
+                username = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+        }
+    }
+}
+
---
+++ addressbook-web-tests/addressbook-web-tests/Groupdata.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace addressbook_web_tests
+{
+    public class GroupData
+    {
+        private string name;
+        private string header = "";
+        private string footer = "";
+
+        public GroupData(string name, string header, string footer) // constructor
+        {
+            this.name = name;
+            this.header = header;
+            this.footer = footer;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Header
+        {
+            get
+            {
+                return header;
+            }
+            set
+            {
+                header = value;
+            }
+        }
+        public string Footer
+        {
+            get
+            {
+                return footer;
+            }
+            set
+            {
+                footer = value;
+            }
+        }
+    }
+}
+
