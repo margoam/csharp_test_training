f8fa71ad4e58b7c450ffc77d07e9818ef2b11975
margoam <mozheliukrita@gmail.com>
2022-10-10 19:22:39 +0200

    Added Application Manager

---
+++ addressbook-web-tests/addressbook-web-tests/appmanager/ApplicationManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace addressbook_web_tests
+{
+    public class ApplicationManager
+    {
+        protected IWebDriver driver;
+        protected string baseURL;// field
+        protected LoginHelper loginHelper;
+        protected NavigationHelper navigation;
+        protected GroupHelper groupHelper;
+        protected ContactHelper contactHelper;
+
+        public ApplicationManager()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "http://localhost/addressbook";
+
+            loginHelper = new LoginHelper(driver);
+            navigation = new NavigationHelper(driver, baseURL);
+            groupHelper = new GroupHelper(driver);
+            contactHelper = new ContactHelper(driver);
+
+        }
+        public void Stop()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public LoginHelper Auth
+        {
+            get
+
+            {
+                return loginHelper;
+            }
+
+        }
+
+        public NavigationHelper Navigation
+        {
+            get
+            {
+                return navigation;
+            }
+        }
+
+        public GroupHelper Groups
+        {
+            get
+            {
+                return groupHelper;
+            }
+        }
+
+        public ContactHelper Contacts
+        {
+            get
+            {
+                return contactHelper;
+            }
+        }
+
+    }
+}
+
