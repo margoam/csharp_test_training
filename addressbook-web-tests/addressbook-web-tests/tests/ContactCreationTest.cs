5753b4f1ff2b12918e0f67a56ee59a778c89cc30
margoam <mozheliukrita@gmail.com>
2022-11-06 21:28:12 +0100

    implemented loading test data from a file

--- addressbook-web-tests/addressbook-web-tests/tests/ContactCreationTest.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/ContactCreationTest.cs
@@ -4,6 +4,8 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
+using Newtonsoft.Json;
+using System.Xml.Serialization;
 using NUnit.Framework;
 using NUnit.Framework.Internal.Execution;
 
@@ -28,7 +30,37 @@ namespace addressbook_web_tests
             return contacts;
         }
 
-        [Test, TestCaseSource("RandomContactDataProvider")]
+        public static IEnumerable<ContactData> ContactDataFromCsvFile()
+        {
+            List<ContactData> contacts = new List<ContactData>();
+            string[] lines = File.ReadAllLines(@"contacts.csv");
+            foreach (string l in lines)
+            {
+                string[] parts = l.Split(',');
+                contacts.Add(new ContactData(parts[0], parts[1])
+                {
+                    Middlename = parts[2],
+                    Nickname = parts[3],
+                    Company = parts[4],
+                    Address = parts[5]
+                });
+            }
+            return contacts;
+        }
+
+        public static IEnumerable<ContactData> ContactDataFromXmlFile()
+        {
+            return (List<ContactData>)
+                new XmlSerializer(typeof(List<ContactData>))
+                    .Deserialize(new StreamReader(@"contacts.xml"));
+        }
+
+        public static IEnumerable<ContactData> ContactDataFromJsonFile()
+        {
+            return JsonConvert.DeserializeObject<List<ContactData>>(File.ReadAllText(@"contacts.json"));
+        }
+
+        [Test, TestCaseSource("ContactDataFromJsonFile")]
         public void ContactCreationTest(ContactData contact)
         {
             List<ContactData> oldContacts = app.Contacts.GetContactList();
